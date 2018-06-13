a59a3626a74ac272077eb49f0618e9cd776eaeb9
Ukabu <emmanuel@pirsch.org>
2018-06-13 13:47:26 -0400

    refactored out shelljs. child_process is simpler and allow to pass arguments without shell interpretor

--- cf.js
+++ cf.js
@@ -1,29 +1,40 @@
 const os = require("os")
-const shell = require("shelljs")
-const validator = require("validator")
+const child_process = require("child_process")
 
 exports.auth = source => {
   if (process.env.NODE_ENV !== "production") {
-    console.log("CF: non-production: assume we are already logged in")
+    console.log("CF: Non-production: assume we are already logged in")
     return
   }
   const useClientCredentials =
-    !validator.isEmpty(source.client_id) &&
-    !validator.isEmpty(source.client_secret)
+    source.client_id &&
+    source.client_id.length > 0 &&
+    source.client_secret &&
+    source.client_secret.length > 0
   const id = useClientCredentials ? source.client_id : source.username
   const secret = useClientCredentials ? source.client_secret : source.password
 
-  const result = shell.exec(
-    `cf auth "${id}" "${secret}" ${
-      useClientCredentials ? "--client-credentials" : ""
-    }`,
-    { silent: true }
-  )
-  if (result.code === 0) {
-    console.log(
-      `CF: authenticated with ${id} (client-credentials: ${useClientCredentials})`
+  try {
+    child_process.execFileSync("cf", ["api", source.api])
+    console.log(`CF: API endpoint set to ${source.api}`)
+  } catch (e) {
+    throw new Error(`CF: Unable to set API endpoint to ${source.api}`)
+  }
+
+  try {
+    child_process.execFileSync(
+      "cf",
+      [
+        "auth",
+        id,
+        secret,
+        useClientCredentials ? "--client-credentials" : null
+      ].filter(n => n)
     )
-  } else {
+    console.log(
+      `CF: Authenticated with ${id} (client-credentials: ${useClientCredentials})`
+    )
+  } catch (e) {
     throw new Error(
       `CF: Unable to authenticate with ${id} (client-credentials: ${useClientCredentials})`
     )
@@ -33,45 +44,63 @@ exports.auth = source => {
 exports.target = ({ organization, space }) => {
   if (!organization || !space) {
     console.log(
-      "CF: organization and/or space not specified. Using target set locally."
+      "CF: Organization and/or space not specified. Using target set locally."
     )
     return
   }
-  shell.exec(`cf target -o "${organization}" -s "${space}"`, {
-    silent: true
-  })
-  console.log(`CF: targeted ${organization}/${space}`)
+  try {
+    child_process.execFileSync("cf", [
+      "target",
+      "-o",
+      organization,
+      "-s",
+      space
+    ])
+    console.log(`CF: Targeted ${organization}/${space}`)
+  } catch (e) {
+    throw new Error(`CF: Unable to target ${organization}/${space}`)
+  }
 }
 
 exports.appInfo = ({ name }) => {
-  const guid = shell.exec(`cf app --guid ${name}`, { silent: true })
-  if (guid.code > 0) {
-    throw new Error(`CF: application '${name}' not found`)
+  try {
+    const guid = child_process
+      .execFileSync("cf", ["app", "--guid", name])
+      .toString()
+      .trim()
+
+    const appInfo = child_process
+      .execFileSync("cf", ["curl", `/v2/apps/${guid}`])
+      .toString()
+
+    return JSON.parse(appInfo)
+  } catch (e) {
+    throw new Error(`CF: Application '${name}' not found`)
   }
-
-  const result = shell.exec(`cf curl /v2/apps/${guid.stdout}`, {
-    silent: true
-  })
-
-  return JSON.parse(result.stdout)
 }
 
 exports.appExists = ({ name }) => {
-  const guid = shell.exec(`cf app --guid ${name}`, { silent: true })
-  return guid.code === 0
+  try {
+    child_process.execFileSync("cf", ["app", "--guid", name])
+    return true
+  } catch (e) {
+    return false
+  }
 }
 
 exports.delete = ({ name }) => {
-  shell.exec(`cf delete "${name}" -f`, { silent: true })
-  console.log(`CF: deleted ${name}`)
+  child_process.execFileSync("cf", ["delete", name, "-f"])
+  console.log(`CF: Deleted ${name}`)
 }
 
 exports.rename = ({ from, to, failOnError = true }) => {
-  const result = shell.exec(`cf rename "${from}" "${to}"`)
-  if (failOnError && result.code > 0) {
-    throw new Error(
-      `CF: Unable to rename ${from} to ${to} : ${os.EOL}${result.stderr}`
-    )
+  try {
+    child_process.execFileSync("cf", ["rename", from, to])
+    console.log(`CF: Renamed ${from} to ${to}`)
+  } catch (e) {
+    if (failOnError) {
+      console.error(`CF: Unable to rename ${from} to ${to}`)
+    }
   }
 }
 
@@ -82,13 +111,18 @@ exports.push = ({ name, path, manifest, docker_password }) => {
     env["CF_DOCKER_PASSWORD"] = docker_password
   }
 
-  const result = shell.exec(`cf push "${name}" -f "${manifest}" -p "${path}"`, {
-    silent: true,
-    env
-  })
-  if (result.code === 0) {
+  try {
+    child_process.execFileSync("cf", [
+      "push",
+      name,
+      "-f",
+      manifest,
+      "-p",
+      path,
+      { env }
+    ])
     console.log(`CF: Application ${name} successfully deployed!`)
-  } else {
+  } catch (e) {
     throw new Error(
       `CF: Unable to deploy ${name}:${os.EOL}${result.stdout}${os.EOL}${
         result.stderr
@@ -98,21 +132,21 @@ exports.push = ({ name, path, manifest, docker_password }) => {
 }
 
 exports.log = ({ name }) => {
-  const result = shell.exec(`cf logs --recent "${name}"`, { silent: true })
-  if (result.code === 0) {
-    return result.stdout
-  } else {
-    throw new Error(
-      `CF: Unable to obtain "${name}" logs:${os.EOL}${result.stderr}`
-    )
+  try {
+    const logs = child_process
+      .execFileSync("cf", ["logs", "--recent", name])
+      .toString()
+    return logs
+  } catch (e) {
+    throw new Error(`CF: Unable to obtain "${name}" logs`)
   }
 }
 
 exports.stop = ({ name }) => {
-  const result = shell.exec(`cf stop "${name}"`)
-  if (result.code === 0) {
+  try {
+    child_process.execFileSync("cf", ["stop", name])
     console.log(`CF: Application "${name} stopped.`)
-  } else {
+  } catch (e) {
     console.warn(`CF: WARNING Unable to stop application "${name}"`)
   }
 }
